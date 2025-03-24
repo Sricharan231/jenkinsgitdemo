f24c16cbdd79607ff8b418e76cad7878deb257ae
Sricharan231 <sricharank2002@gmail.com>
2025-03-24 11:24:05 +0000

    first commit

---
+++ src/main/java/com/ey/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.ey.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class HomeController {
+	
+	@GetMapping("/")
+	public String home() {
+		return "Welcome Home";
+	}
+	@GetMapping("/page1")
+	public String page1() {
+		return "Welcome to page 1";
+	}
+	
+
+}
